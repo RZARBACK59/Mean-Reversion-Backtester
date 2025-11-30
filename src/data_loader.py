b9d634c591743d0ffed829ab6e776ec6b06c3961
RZARBACK59 <morris.jg01@gmail.com>
2025-11-30 22:55:04 +0000

    removing yfinance warning message that was annoying me

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,5 +4,5 @@ import yfinance as yf
 import pandas as pd
 
 def get_data(ticker, start="2015-01-01"):
-    data = yf.download(ticker, start=start)
+    data = yf.download(ticker, start=start, auto_adjust=True)
     return data
